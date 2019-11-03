4ce636d2f5e8a54b6b675aa1deb96aa46fd08277
Ambrance <ambrance@mail.ru>
2019-11-03 12:05:44 +0300

    feat: add keyboard and mouse actions

--- codejam-virtual-keyboard/src/script.js
+++ codejam-virtual-keyboard/src/script.js
@@ -1,103 +1,201 @@
-/* const keyBoardKeys = {
-  backspace: 8,
-  tab: 9,
-  enter: 13,
-  shift: 16,
-  ctrl: 17,
-  alt: 18,
-  capslock: 20,
-  escape: 27,
-  arrowleft: 37,
-  arrowup: 38,
-  arrowright: 39,
-  arrowdown: 40,
-  0: 48,
-  1: 49,
-  2: 50,
-  3: 51,
-  4: 52,
-  5: 53,
-  6: 54,
-  7: 55,
-  8: 56,
-  9: 57,
-  a: 65,
-  b: 66,
-  c: 67,
-  d: 68,
-  e: 69,
-  f: 70,
-  g: 71,
-  h: 72,
-  i: 73,
-  j: 74,
-  k: 75,
-  l: 76,
-  m: 77,
-  n: 78,
-  o: 79,
-  p: 80,
-  q: 81,
-  r: 82,
-  s: 83,
-  t: 84,
-  u: 85,
-  v: 86,
-  w: 87,
-  x: 88,
-  y: 89,
-  z: 90,
-  semicolon: 186,
-  equalsign: 187,
-  comma: 188,
-  minus: 189,
-  period: 190,
-  slash: 191,
-  backquote: 192,
-  bracketleft: 219,
-  backslash: 220,
-  braketright: 221,
-  quote: 222,
-}; */
+class Keyboard {
+  constructor() {
+    this.layoutEng = {
+      keyBoardEngSigns: [
+        '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+        'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'backslash',
+        'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
+        'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
+        'ctrl', 'win', 'alt', 'space', 'alt', '◄', '▼', '►', 'ctrl',
+      ],
+      keys: {
+        AltLeft: 'Alt',
+        AltRight: 'Alt',
+        ArrowDown: 'ArrowDown',
+        ArrowLeft: 'ArrowLeft',
+        ArrowRight: 'ArrowRight',
+        ArrowUp: 'ArrowUp',
+        Backquote: '`',
+        Backslash: '\\',
+        Backspace: 'Backspace',
+        BracketLeft: '[',
+        BracketRight: ']',
+        CapsLock: 'CapsLock',
+        Comma: ',',
+        ControlLeft: 'Control',
+        ControlRight: 'Control',
+        Digit0: '0',
+        Digit1: '1',
+        Digit2: '2',
+        Digit3: '3',
+        Digit4: '4',
+        Digit5: '5',
+        Digit6: '6',
+        Digit7: '7',
+        Digit8: '8',
+        Digit9: '9',
+        Enter: 'Enter',
+        Equal: '=',
+        KeyA: 'a',
+        KeyB: 'b',
+        KeyC: 'c',
+        KeyD: 'd',
+        KeyE: 'e',
+        KeyF: 'f',
+        KeyG: 'g',
+        KeyH: 'h',
+        KeyI: 'i',
+        KeyJ: 'j',
+        KeyK: 'k',
+        KeyL: 'l',
+        KeyM: 'm',
+        KeyN: 'n',
+        KeyO: 'o',
+        KeyP: 'p',
+        KeyQ: 'q',
+        KeyR: 'r',
+        KeyS: 's',
+        KeyT: 't',
+        KeyU: 'u',
+        KeyV: 'v',
+        KeyW: 'w',
+        KeyX: 'ч',
+        KeyY: 'y',
+        KeyZ: 'z',
+        Minus: '-',
+        Period: '.',
+        Quote: '\'',
+        Semicolon: ';',
+        ShiftLeft: 'Shift',
+        ShiftRight: 'Shift',
+        Slash: '/',
+        Space: ' ',
+        Tab: 'Tab',
+      },
+    };
+    this.layoutRus = {
+      keyBoardRusSigns: [
+        'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+        'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'backslash',
+        'capslock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
+        'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'shift',
+        'ctrl', 'win', 'alt', 'space', 'alt', '◄', '▼', '►', 'ctrl',
+      ],
+      keys: {
+        AltLeft: 'Alt',
+        AltRight: 'Alt',
+        ArrowDown: 'ArrowDown',
+        ArrowLeft: 'ArrowLeft',
+        ArrowRight: 'ArrowRight',
+        ArrowUp: 'ArrowUp',
+        Backquote: 'ё',
+        Backslash: '\\',
+        Backspace: 'Backspace',
+        BracketLeft: 'х',
+        BracketRight: 'ъ',
+        CapsLock: 'CapsLock',
+        Comma: 'б',
+        ControlLeft: 'Control',
+        ControlRight: 'Control',
+        Digit0: '0',
+        Digit1: '1',
+        Digit2: '2',
+        Digit3: '3',
+        Digit4: '4',
+        Digit5: '5',
+        Digit6: '6',
+        Digit7: '7',
+        Digit8: '8',
+        Digit9: '9',
+        Enter: 'Enter',
+        Equal: '=',
+        KeyA: 'ф',
+        KeyB: 'и',
+        KeyC: 'с',
+        KeyD: 'в',
+        KeyE: 'у',
+        KeyF: 'а',
+        KeyG: 'п',
+        KeyH: 'р',
+        KeyI: 'ш',
+        KeyJ: 'о',
+        KeyK: 'л',
+        KeyL: 'д',
+        KeyM: 'ь',
+        KeyN: 'т',
+        KeyO: 'щ',
+        KeyP: 'з',
+        KeyQ: 'й',
+        KeyR: 'к',
+        KeyS: 'ы',
+        KeyT: 'е',
+        KeyU: 'г',
+        KeyV: 'м',
+        KeyW: 'ц',
+        KeyX: 'ч',
+        KeyY: 'н',
+        KeyZ: 'я',
+        Minus: '-',
+        Period: 'ю',
+        Quote: 'э',
+        Semicolon: 'ж',
+        ShiftLeft: 'Shift',
+        ShiftRight: 'Shift',
+        Slash: '.',
+        Space: ' ',
+        Tab: 'Tab',
+      },
+    };
 
-const keyBoardSigns = [
-  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-  'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-  'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
-  'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
-  'ctrl', 'win', 'alt', 'space', 'alt', '◄', '▼', '►', 'ctrl',
-];
+    this.mode = this.layoutEng;
+    this.signs = this.mode.keyBoardEngSigns;
+    this.keys = this.mode.keys;
+    console.log(this.keys);
+  }
+
+  render(parent, addClass) {
+    const serviceKeys = ['backspace', 'tab', 'backslash', 'space', 'capslock', 'enter', 'shift'];
+    for (let i = 0; i < this.signs.length; i += 1) {
+      const button = document.createElement('button');
+      button.classList.add(addClass);
+      button.textContent = this.signs[i];
+      if (serviceKeys.some((elem) => button.textContent === elem)) {
+        button.className += ` ${addClass}--${button.textContent}`;
+      }
+      parent.append(button);
+    }
+  }
+}
+const inputField = document.createElement('textarea');
+inputField.classList.add('input-field');
+document.body.append(inputField);
 const keyBoardBox = document.createElement('div');
 keyBoardBox.classList.add('keyboard-container');
 document.body.append(keyBoardBox);
-for (let i = 0; i < keyBoardSigns.length; i += 1) {
-  const button = document.createElement('button');
-  button.classList.add('keyboard-item');
-  button.textContent = keyBoardSigns[i];
-  switch (button.textContent) {
-    case 'backspace':
-      button.className += ' keyboard-item--backspace';
-      break;
-    case 'tab':
-      button.className += ' keyboard-item--tab';
-      break;
-    case '\\':
-      button.className += ' keyboard-item--slash';
-      break;
-    case 'space':
-      button.className += ' keyboard-item--space';
-      break;
-    case 'capslock':
-      button.className += ' keyboard-item--capslock';
-      break;
-    case 'enter':
-      button.className += ' keyboard-item--enter';
-      break;
-    case 'shift':
-      button.className += ' keyboard-item--shift';
-      break;
-    default:
-      break;
+const myKeyboard = new Keyboard();
+myKeyboard.render(keyBoardBox, 'keyboard-item');
+const buttonsCollection = keyBoardBox.getElementsByClassName('keyboard-item');
+const buttonsArr = Array.from(buttonsCollection);
+const enterSigns = (evt) => {
+  evt.preventDefault();
+  const target = buttonsArr.find((button) => button.textContent === myKeyboard.keys[evt.code]);
+  console.log(myKeyboard.keys[evt.code]);
+  target.classList.add('keyboard-item--active');
+  inputField.value += target.textContent;
+};
+document.addEventListener('keydown', enterSigns);
+document.addEventListener('keyup', (evt) => {
+  const target = buttonsArr.find((button) => button.textContent === myKeyboard.keys[evt.code]);
+  target.classList.remove('keyboard-item--active');
+});
+document.addEventListener('mousedown', (evt) => {
+  if (evt.target.tagName === 'BUTTON') {
+    evt.target.classList.add('keyboard-item--active');
+    inputField.value += evt.target.textContent;
   }
-  keyBoardBox.append(button);
-}
+});
+document.addEventListener('mouseup', (evt) => {
+  if (evt.target.tagName === 'BUTTON') {
+    evt.target.classList.remove('keyboard-item--active');
+  }
+});
